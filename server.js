87b1c0f4db822a0f305ee9bec429389346d04e9e
Carl Hain <crhain@gmail.com>
2017-04-20 15:29:07 -0500

    Add route 'api/whoami' and basic header parsing

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const port = process.argv[2] || 8080;
 app.listen(port);
 console.log('running server on port: ' + port);
 
-app.get('/', (request, response) =>{
-    response.end('Hola!');
+app.get('/api/whoami', (request, response) =>{
+    let jsonObject = {ipaddress: null, language: null, software: null }
+    //let header = request.header;
+    let ip = request.ip;
+    let language = request.header('accept-language');
+    let software = request.header('user-agent');    
+    
+    jsonObject.ipaddress = ip;
+    //Parse out language using regex XXX,    
+    jsonObject.language = language;
+    //Parse out sytem-information using regex: User-Agent: Mozilla/<version> (<system-information>) <platform> (<platform-details>) <extensions>
+    jsonObject.software = software;
+    
+    console.log(request.headers);
+    response.end(JSON.stringify(jsonObject));
 });
